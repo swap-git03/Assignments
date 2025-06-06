6c8c71104d133d9470581476e705eef6818cc598
swap-git03 <swapnilfulpagare061003@gmail.com>
2025-06-06 22:21:50 +0530

    odd even done

--- Assignment-18-Operators-Expressions/operatorsexpress.js
+++ Assignment-18-Operators-Expressions/operatorsexpress.js
@@ -1,1 +1,8 @@
-// wait i will complete soon
+// to check odd even
+ let num = prompt('enter number');
+    if (num % 2 ===0){
+        console.log('number is even')
+    } else {
+        console.log('number is odd')
+
+    }
