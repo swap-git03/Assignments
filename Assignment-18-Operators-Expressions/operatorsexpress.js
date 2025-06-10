00be5dd01e9b0e4ac24f06d98ca01ff087a283bd
swap-git03 <swapnilfulpagare061003@gmail.com>
2025-06-10 20:47:14 +0530

    working!

--- Assignment-18-Operators-Expressions/operatorsexpress.js
+++ Assignment-18-Operators-Expressions/operatorsexpress.js
@@ -1,15 +1,19 @@
-// to check odd even
+// inputs 
  let num1 = prompt('enter number 1');
  let num2 = prompt('enter number 2');
-    if (num1 % 2 ===0){
-        console.log('number is even')
-    } else {
-        console.log('number is odd')
-
-    }
 // 1. arithmatic operations
 
 console.log('addition is', num1 + num2);
 console.log('division is', num1 / num2);
 console.log('modulus is', num1 % num2);
 console.log('exponential is', num1 ** num2);
+
+
+// 9. Check even or odd
+
+    if (num1 % 2 ===0){
+        console.log('number is even')
+    } else {
+        console.log('number is odd')
+
+    }
