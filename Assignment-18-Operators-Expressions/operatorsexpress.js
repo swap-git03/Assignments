11c1c46301a77e9b9cd4e7e8c93eec5a66200c64
swap-git03 <swapnilfulpagare061003@gmail.com>
2025-06-10 20:53:42 +0530

    wait!!

--- Assignment-18-Operators-Expressions/operatorsexpress.js
+++ Assignment-18-Operators-Expressions/operatorsexpress.js
@@ -9,11 +9,22 @@ console.log('modulus is', num1 % num2);
 console.log('exponential is', num1 ** num2);
 
 
+// 2. 
+console.log("if the first number greater than second", num1 > num2);
+console.log("if the first number less than second", num1 > num2);
+console.log("if the first number equal to second", num1 === num2);
+
+
+// 4, declaring X;
+
+const x = 10;
+ x+=5;
+console.log(x);
+
 // 9. Check even or odd
 
     if (num1 % 2 ===0){
         console.log('number is even')
     } else {
         console.log('number is odd')
-
     }
