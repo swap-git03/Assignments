c57d12bee387f6bb5cf7c95841b698ce5c48c767
swap-git03 <swapnilfulpagare061003@gmail.com>
2025-07-02 16:49:51 +0530

    5 done!

--- Assignment-18-Operators-Expressions/operatorsexpress.js
+++ Assignment-18-Operators-Expressions/operatorsexpress.js
@@ -43,7 +43,17 @@ console.log(x);
 
 
 
-// Operator Precidence
+// 5. Operator Precidence
+let expr1 = (10 + 3 - 5);
+let expr2 = (10 + 3) * 5 ;
+let expr3 = 20 / 2 + 3;
+let expr4 = 20 / (2 + 3);
+
+console.log(expr1);
+console.log(expr2);
+console.log(expr3);
+console.log(expr4);
+
 
 
 
