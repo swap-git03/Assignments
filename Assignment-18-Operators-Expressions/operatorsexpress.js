53d267ed8a38a27ebc19ef0dcd95db7ac495d91c
swap-git03 <swapnilfulpagare061003@gmail.com>
2025-07-02 16:08:25 +0530

    .

--- Assignment-18-Operators-Expressions/operatorsexpress.js
+++ Assignment-18-Operators-Expressions/operatorsexpress.js
@@ -15,6 +15,11 @@ console.log("if the first number less than second", num1 > num2);
 console.log("if the first number equal to second", num1 === num2);
 
 
+// 3.
+let isEligible = (num1 >= 90) && (num2 >= 85);
+console.log(isEligible);
+
+
 // 4, declaring X;
 
 const x = 10;
